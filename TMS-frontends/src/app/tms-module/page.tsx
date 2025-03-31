221d362e794d01e06ca86d57668a5ed3ab63fc1d
Amlakab <amlakieabebaw96@gmail.com>
2025-03-31 14:21:27 +0300

    delete

--- TMS-frontends/src/app/tms-module/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TMSModulePage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Transport Management System</h1>
-        {/* Y our page content here */}
-      </div>
-    );
-  }
